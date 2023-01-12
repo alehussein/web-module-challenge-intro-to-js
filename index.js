0025fca6373a6867433279475654ad5d543e8bfa
Alejandro Hussein <alejandrohussein@Nelmas-MBP.attlocal.net>
2023-01-12 17:52:59 -0600

    hungrydogs

--- index.js
+++ index.js
@@ -142,16 +142,17 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(age, weight){
-  if (age >= 1 && weight <= 5){return weight * 0.05;}
-  else if(age >= 1 && weight >= 6 && weight <= 10){return weight * 0.04;}
-  else if(age >= 1 && weight >= 11 && weight <= 15){return weight * 0.03;}
-  else if(age >= 1 && weight > 15){return weight * 0.02;}
-  else if(age < 1 && age >= 0.583){return weight * 0.04;}
-  else if(age < 0.583 && age >= 0.333){return weight * 0.05;}
-  else if(age < 0.333){return weight * 0.10;}
+function hungryDog(weight, age){
+  
+   if (age >= 1 && weight <= 5){return weight * 0.05;}
+   else if(age >= 1 && weight >= 6 && weight <= 10){return weight * 0.04;}
+   else if(age >= 1 && weight >= 11 && weight <= 15){return weight * 0.03;}
+   else if(age >= 1 && weight > 15){return weight * 0.02;}
+   else if(age < 1 && age >= 0.583){return weight * 0.04;}
+   else if(age < 0.583 && age >= 0.333){return weight * 0.05;}
+   else if(age < 0.333){return weight * 0.10;}
 }
-console.log("Task 3", hungryDog(1, 15));
+console.log("Task 3", hungryDog(15, 1));
 
 
 
